cca1482443c0884b8f1510db4fbe7c4f2c021b9d
DThiwanka <it20129576@my.sliit.lk>
2024-02-13 12:52:46 +0530

    Add weapon property to CreateNinjaDto and update NinjasController and NinjasService

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,51 +1,44 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { NinjasService } from './ninjas.service';
 
 @Controller('ninjas')
 export class NinjasController {
 
+    constructor(private readonly NinjasService: NinjasService) { }
+
 
     // Get/ninjas ---> []
     @Get()
-    getNinjas(@Query('type') type:string) {
-        return [
-            {type}
-        ];
+    getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks') {
+        //const service = new NinjasService();
+        return this.NinjasService.getNinjas(weapon);
     }
 
     //get/ninjas/:id ---> {.....}
 
     @Get(':id')
     GetOneNinja(@Param('id') id: string) {
-        return {
-            id
-        };
+        return this.NinjasService.getNinja(+id);
     }
 
     //post /ninjas
     @Post()
-    createNinja(@Body() CreateNinjaDto:CreateNinjaDto) {
-        return {
-            name:CreateNinjaDto.name
-        };
+    createNinja(@Body() CreateNinjaDto: CreateNinjaDto) {
+        return this.NinjasService.createNinja(CreateNinjaDto);
     }
 
     //put /ninjas/:id ----> {......}
     @Put(':id')
-    updateNinja(@Param('id') id:string, @Body() UpdateNinjaDto:UpdateNinjaDto) {
-        return {
-            id,
-            name:UpdateNinjaDto.name,
-        };
+    updateNinja(@Param('id') id: string, @Body() UpdateNinjaDto: UpdateNinjaDto) {
+        return this.NinjasService.updateNinja(+id, UpdateNinjaDto);
     }
 
     //Delete /ninjas/:id
     @Delete(':id')
-    removeNinja(@Param('id') id:string) {
-        return {
-            id
-        };
+    removeNinja(@Param('id') id: string) {
+        return this.NinjasService.removeNinja(+id);
     }
 }
 
